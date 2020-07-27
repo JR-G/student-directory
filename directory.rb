4123e02e58d73df66d554042844ac4fe00bf2db3
JRG <47917431+JR-G@users.noreply.github.com>
2020-07-27 17:47:22 +0100

    Refactored the list of students

--- directory.rb
+++ directory.rb
@@ -12,21 +12,41 @@ students = [
     {name: "Norman Bates", cohort: :november}
   ]
 
-def print_header
-    puts "The sudents of Villains Academy"
+  def print_header
+    puts "The students of Villains Academy"
     puts "-------------"
-end
+  end
 
-def print(students)
+  def print(students)
     students.each do |student|
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end    
+    end
+  end
+
+  def print_footer(students)
+    puts "Overall, we have #{students.count} great students"
+  end
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create an empty array
+    students = []
+    # get the first name
+    name = gets.chomp
+    # whilt the name is not empty, repeat this code
+    while !name.empty? do
+        # add the student has to the array
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        # get another name from the user
+        name = gets.chomp
+    end
+    # return the array of students
+    students
 end
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
-end
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
