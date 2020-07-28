c42cb5e305b455135137ec04cfa5cf0b243ab167
JRG <47917431+JR-G@users.noreply.github.com>
2020-07-28 17:18:34 +0100

    Added save_students method

--- directory.rb
+++ directory.rb
@@ -18,13 +18,7 @@ def input_students
 				puts "Please re-enter their cohort:"
 				cohort = gets.strip
 			end
-      puts "Please enter their hobbies:"
-      hobbies = gets.strip
-      puts "Please enter their country of birth:"
-      birth_country = gets.strip
-      puts "Please enter their height:"
-      height = gets.strip
-			@students << {name: name, cohort: cohort, hobbies: hobbies, country: birth_country, height: height}
+			@students << {name: name, cohort: cohort}
 			if @students.count == 1
 				puts"Now we have #{@students.count} student"
 			else
@@ -45,6 +39,7 @@ end
 	def print_menu
 		puts "1. Input the students"
 		puts "2. Show the students"
+		puts "3. Save the list to students.csv"
 		puts "9. Exit"
 	end
 
@@ -54,12 +49,26 @@ end
 		print_footer
 	end
 
+	def save_students
+		# open the file for writing
+		file = File.open("students.csv", "w")
+		# iterate over the array of students
+		@students.each do |student|
+			student_data = [student[:name], student[:cohort]]
+			csv_line = student_data.join(",")
+			file.puts csv_line
+		end
+		file.close
+	end
+
 	def process(selection)
 		case selection
 		when "1"
 			input_students
 		when "2"
 			show_students
+		when "3"
+			save_students
 		when "9"
 			exit
 		else
@@ -77,7 +86,7 @@ end
 			puts "...There are no students"
 		end
     @students.each_with_index do |student, index|
-				puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort). Their country of birth is: #{student[:country]} and their hobbies include: #{student[:hobbies]}. They are #{student[:height]} tall."
+				puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)."
 		end
 	end
 
